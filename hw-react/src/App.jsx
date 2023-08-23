11c13c1c8a1281dc2647adb9b5eb57fd9b654432
Ganesh Janamanchi <ganeshjanamanchi@gmail.com>
2023-08-23 19:36:15 +0530

    Resuable Component - Button, and Everything is in its own file

--- hw-react/src/App.jsx
+++ hw-react/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import Button from "./Button";
 
 const Appy = () => {
   /*
         Cannot return more than one element from this return.
         If not error: JSX expressions must have one parent element
         Hence wrap all the elements into one element (ex. div)
+
+        * And also comments need to be put inside curly braces in jsx (●'◡'●)
         */
   return (
     <div>
@@ -13,13 +16,9 @@ const Appy = () => {
       style properties are camelCase insted of text-align
               */}
       <h1 style={{ textAlign: "center" }}>Hello World !!</h1>
-      {/*
-          <button class="button">Yo!</button>
-        * 'class' is a reserved keyword here, we need to be using className
-        * If not warning in console: Invalid DOM property `class`. Did you mean `className`? at button
-        * And also comments need to be put inside curly braces in jsx (●'◡'●)
-              */}
-      <button className="button">Yo!</button>
+      <Button title="Button 1 ☜(ﾟヮﾟ☜)" />
+      <Button title="(☞ﾟヮﾟ)☞ Button 2" />
+      <Button />
     </div>
   );
 };
